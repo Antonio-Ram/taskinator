39fd8479c4bf79ce1911b04a8aa7d70815b304dc
Antonio <tonito@umich.edu>
2021-11-05 19:18:22 -0400

    Fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,14 @@ var taskFormHandler = function (event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if(!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -31,7 +39,7 @@ var createTaskEl = function (taskDataObj) {
     listItemEl.appendChild(taskInfoEl);
 
     //add entire list item to list
-    tasksToDo.appendChild(listItemEl);
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", taskFormHandler);
